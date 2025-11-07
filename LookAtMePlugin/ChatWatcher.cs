a7b6eae38524e9ec7eb992b8d5ae40f5d9a952e1
echodetla <242634456+echodetla@users.noreply.github.com>
2025-11-07 18:08:28 +0100

    :bug: fixed crash on system message handling

--- LookAtMePlugin/ChatWatcher.cs
+++ LookAtMePlugin/ChatWatcher.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
@@ -33,64 +35,83 @@ public class ChatWatcher : IDisposable
     private void ChatOnChatMessage(
         XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled)
     {
-        if (isHandled)
-            return;
-
-        //todo possibly do this on the main plugin and remove the entire watcher if disabled
-        if (!lookAtMePlugin.Configuration.IsEnabled)
-            return;
-
-        if (lookAtMePlugin.InPvPZone)
-            return;
-
-        if (lookAtMePlugin.CachedLocalPlayer?.StatusFlags.HasFlag(StatusFlags.InCombat) ?? false)
+        try
         {
-            return;
-        }
+            if (isHandled)
+                return;
 
-        var enabled = type switch
-        {
-            XivChatType.Say => lookAtMePlugin.Configuration.MonitorPublicChat,
-            XivChatType.Yell => lookAtMePlugin.Configuration.MonitorPublicYellChat,
-            XivChatType.Shout => lookAtMePlugin.Configuration.MonitorPublicShoutChat,
-            XivChatType.CustomEmote => lookAtMePlugin.Configuration.MonitorPublicChat,
-            XivChatType.StandardEmote => lookAtMePlugin.Configuration.MonitorPublicChat,
-            XivChatType.Party => lookAtMePlugin.Configuration.MonitorParty,
-            XivChatType.FreeCompany => lookAtMePlugin.Configuration.MonitorFc,
-            XivChatType.CrossLinkShell1 => lookAtMePlugin.Configuration.MonitorCwl1,
-            XivChatType.CrossLinkShell2 => lookAtMePlugin.Configuration.MonitorCwl2,
-            XivChatType.CrossLinkShell3 => lookAtMePlugin.Configuration.MonitorCwl3,
-            XivChatType.CrossLinkShell4 => lookAtMePlugin.Configuration.MonitorCwl4,
-            XivChatType.CrossLinkShell5 => lookAtMePlugin.Configuration.MonitorCwl5,
-            XivChatType.CrossLinkShell6 => lookAtMePlugin.Configuration.MonitorCwl6,
-            XivChatType.CrossLinkShell7 => lookAtMePlugin.Configuration.MonitorCwl7,
-            XivChatType.CrossLinkShell8 => lookAtMePlugin.Configuration.MonitorCwl8,
-            _ => false
-        };
+            //todo possibly do this on the main plugin and remove the entire watcher if disabled
+            if (!lookAtMePlugin.Configuration.IsEnabled)
+                return;
 
-        if (!enabled)
-            return;
-
-        log.Debug("Processing message of type: {type} from sender: {sender}", type, sender.TextValue);
-
-        var playerPayload = sender.Payloads.FirstOrDefault(x => x.Type == PayloadType.Player) as PlayerPayload;
-        if (playerPayload == null)
-        {
-            //play payload not existing means we can't deduce a player name or more probably its ourself
-            return;
-        }
-
-
-        if (playerPayload.PlayerName != string.Empty)
-        {
-            var senderObject = objectTable.PlayerObjects.FirstOrDefault(x => x.Name.TextValue == playerPayload.PlayerName);
-            if (senderObject == null)
+            var enabled = type switch
             {
+                XivChatType.Say => lookAtMePlugin.Configuration.MonitorPublicChat,
+                XivChatType.Yell => lookAtMePlugin.Configuration.MonitorPublicYellChat,
+                XivChatType.Shout => lookAtMePlugin.Configuration.MonitorPublicShoutChat,
+                XivChatType.CustomEmote => lookAtMePlugin.Configuration.MonitorPublicChat,
+                XivChatType.StandardEmote => lookAtMePlugin.Configuration.MonitorPublicChat,
+                XivChatType.Party => lookAtMePlugin.Configuration.MonitorParty,
+                XivChatType.FreeCompany => lookAtMePlugin.Configuration.MonitorFc,
+                XivChatType.CrossLinkShell1 => lookAtMePlugin.Configuration.MonitorCwl1,
+                XivChatType.CrossLinkShell2 => lookAtMePlugin.Configuration.MonitorCwl2,
+                XivChatType.CrossLinkShell3 => lookAtMePlugin.Configuration.MonitorCwl3,
+                XivChatType.CrossLinkShell4 => lookAtMePlugin.Configuration.MonitorCwl4,
+                XivChatType.CrossLinkShell5 => lookAtMePlugin.Configuration.MonitorCwl5,
+                XivChatType.CrossLinkShell6 => lookAtMePlugin.Configuration.MonitorCwl6,
+                XivChatType.CrossLinkShell7 => lookAtMePlugin.Configuration.MonitorCwl7,
+                XivChatType.CrossLinkShell8 => lookAtMePlugin.Configuration.MonitorCwl8,
+                _ => false
+            };
+
+            if (!enabled)
+                return;
+
+
+            if (lookAtMePlugin.InPvPZone)
+                return;
+
+            if (lookAtMePlugin.CachedLocalPlayer != null)
+            {
+                if (InCombat(lookAtMePlugin.CachedLocalPlayer!))
+                {
+                    return;
+                }
+            }
+
+
+#if DEBUG
+            log.Debug("Processing message of type: {type} from sender: {sender}", type, sender.TextValue);
+# endif
+
+            var playerPayload = sender.Payloads.FirstOrDefault(x => x.Type == PayloadType.Player) as PlayerPayload;
+            if (playerPayload == null)
+            {
+                //play payload not existing means we can't deduce a player name or more probably its ourself
                 return;
             }
-            targetManager.Target = senderObject;
+
+
+            if (playerPayload.PlayerName != string.Empty)
+            {
+                var senderObject =
+                    objectTable.PlayerObjects.FirstOrDefault(x => x.Name.TextValue == playerPayload.PlayerName);
+                if (senderObject == null)
+                {
+                    return;
+                }
+
+                targetManager.Target = senderObject;
+            }
+        }
+        catch (Exception ex)
+        {
+            log.Error($"Error in ChatOnChatMessage: {ex}");
         }
     }
+
+    private static bool InCombat(IGameObject actor) =>
+        actor is IPlayerCharacter pc && pc.StatusFlags.HasFlag(StatusFlags.InCombat);
 
 
     public void Dispose()
--- LookAtMePlugin/LookAtMePlugin.cs
+++ LookAtMePlugin/LookAtMePlugin.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
 using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using LookAtMePlugin.Windows;
-using Lumina.Excel.Sheets.Experimental;
 
 namespace LookAtMePlugin;
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public sealed class LookAtMePlugin : IDalamudPlugin
 {
+    // Plugin services
     [PluginService]
     internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
 
@@ -26,7 +25,7 @@ public sealed class LookAtMePlugin : IDalamudPlugin
 
     [PluginService]
     internal static IObjectTable ObjectTable { get; private set; } = null!;
-    
+
     [PluginService]
     internal static ITargetManager TargetManager { get; private set; } = null!;
 
@@ -39,107 +38,54 @@ public sealed class LookAtMePlugin : IDalamudPlugin
     [PluginService]
     internal static IFramework Framework { get; private set; } = null!;
 
+    // Constants
     private const string SettingsCommandName = "/lookatme";
     private const string ToggleCommandName = "/togglelookatme";
 
+    // Properties
     public Configuration Configuration { get; init; }
-
     public readonly WindowSystem WindowSystem = new("LookAtMePlugin");
     private ConfigWindow ConfigWindow { get; init; }
-
     private ChatWatcher Watcher { get; init; }
     public IPlayerCharacter? CachedLocalPlayer { get; set; }
-    
     public bool InPvPZone { get; private set; }
 
+    // Constructor
     public LookAtMePlugin()
     {
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-
         ConfigWindow = new ConfigWindow(this);
-
         WindowSystem.AddWindow(ConfigWindow);
 
         CommandManager.AddHandler(SettingsCommandName, new CommandInfo(OnConfigCommand)
         {
             HelpMessage = "Opens the settings window"
         });
-        
         CommandManager.AddHandler(ToggleCommandName, new CommandInfo(OnToggleCommand)
         {
             HelpMessage = "Toggles the functionality of look at me"
         });
 
-
-        // Tell the UI system that we want our windows to be drawn throught he window system
         PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
-
         Watcher = new ChatWatcher(this, Chat, TargetManager, ObjectTable, Log);
-
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // toggling the display status of the configuration ui
         PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
 
         ClientState.Login += OnClientStateOnLogin;
-        ClientState.Logout += OnClientStateOnLogout;
         ClientState.EnterPvP += ClientStateOnEnterPvP;
         ClientState.LeavePvP += ClientStateOnLeavePvP;
         Framework.Update += OnFrameworkOnUpdate;
+    }
+
+    // Event Handlers
+    private void OnConfigCommand(string command, string args)
+    {
+        ToggleConfigUi();
     }
 
     private void OnToggleCommand(string command, string arguments)
     {
         Configuration.IsEnabled = !Configuration.IsEnabled;
         Configuration.Save();
-    }
-
-    private void ClientStateOnLeavePvP()
-    {
-        InPvPZone = false;
-    }
-
-    private void ClientStateOnEnterPvP()
-    {
-        InPvPZone = true;
-    }
-
-    public void Dispose()
-    {
-        // Unregister all actions to not leak anything during disposal of plugin
-        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
-        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
-
-        WindowSystem.RemoveAllWindows();
-
-        ClientState.Login -= OnClientStateOnLogin;
-        ClientState.Logout -= OnClientStateOnLogout;
-        ClientState.EnterPvP -= ClientStateOnEnterPvP;
-        ClientState.LeavePvP -= ClientStateOnLeavePvP;
-        Framework.Update -= OnFrameworkOnUpdate;
-        
-        ConfigWindow.Dispose();
-        Watcher.Dispose();
-
-        CommandManager.RemoveHandler(SettingsCommandName);
-    }
-
-    private void OnFrameworkOnUpdate(IFramework framework)
-    {
-        if (CachedLocalPlayer == null)
-        {
-            var player = framework.RunOnFrameworkThread(() => ClientState.LocalPlayer).Result;
-            if (player != null)
-            {
-                CachedLocalPlayer = player;
-                Framework.Update -= OnFrameworkOnUpdate;
-            }
-        }
-    }
-
-    private void OnClientStateOnLogout(int type, int code)
-    {
-        CachedLocalPlayer = null;
-        Framework.Update += OnFrameworkOnUpdate;
     }
 
     private void OnClientStateOnLogin()
@@ -150,11 +96,58 @@ public sealed class LookAtMePlugin : IDalamudPlugin
             CachedLocalPlayer = player;
             Framework.Update -= OnFrameworkOnUpdate;
         }
+        else
+        {
+            Framework.Update += OnFrameworkOnUpdate;
+        }
     }
-    
-    private void OnConfigCommand(string command, string args)
+
+    private void ClientStateOnEnterPvP()
     {
-        ToggleConfigUi();
+        InPvPZone = true;
     }
+
+    private void ClientStateOnLeavePvP()
+    {
+        InPvPZone = false;
+    }
+
+    private void OnFrameworkOnUpdate(IFramework framework)
+    {
+        try
+        {
+            if (CachedLocalPlayer == null)
+            {
+                var player = framework.RunOnFrameworkThread(() => ClientState.LocalPlayer).Result;
+                if (player != null)
+                {
+                    CachedLocalPlayer = player;
+                    Framework.Update -= OnFrameworkOnUpdate;
+                }
+            }
+        } catch (Exception ex)
+        {
+            Log.Error($"Error in OnFrameworkOnUpdate: {ex}");
+        }
+    }
+
+    // Public Methods
     public void ToggleConfigUi() => ConfigWindow.Toggle();
+
+    // Dispose
+    public void Dispose()
+    {
+        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
+        WindowSystem.RemoveAllWindows();
+
+        ClientState.Login -= OnClientStateOnLogin;
+        ClientState.EnterPvP -= ClientStateOnEnterPvP;
+        ClientState.LeavePvP -= ClientStateOnLeavePvP;
+        Framework.Update -= OnFrameworkOnUpdate;
+
+        ConfigWindow.Dispose();
+        Watcher.Dispose();
+        CommandManager.RemoveHandler(SettingsCommandName);
+    }
 }
